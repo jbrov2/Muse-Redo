4d757f616b4f279d9a45a4163b5f6ee7ce09460c
jbrov2 <jafaribrownsha@gmail.com>
2024-06-26 12:58:04 -0400

    typo

--- client/src/components/landing_page/HeroSection.jsx
+++ client/src/components/landing_page/HeroSection.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <div className="container mx-auto h-full flex items-center justify-center xl:justify-start relative z-20">
           <div className="hero_text w-full maw-w-[567] flex flex-col items-center xl:items-start text-center xl:text-left relative z-30">
             <h1 className="header_title h1 mb-8 hover:text-accent cursor-pointer transition-all duration-500">
-              Muse
+              muse
             </h1>
             <p className="mb-8">
               The bridge between{" "}
