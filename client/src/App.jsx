686dbf40137beea18f80aa93028149f455468338
jbrov2 <jafaribrownsha@gmail.com>
2024-06-28 13:58:50 -0400

    added login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import SignUp from "./views/SignUp";
+import Login from "./components/User-Auth/login";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
     </>
