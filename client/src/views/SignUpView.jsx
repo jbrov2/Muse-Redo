c42ae37871bc8fd40d832cbe1eb48442726e296b
zephyr <jafaribrownsha@gmail.com>
2024-06-30 18:33:47 -0400

    changed the name of the file

---
+++ client/src/views/SignUpView.jsx
@@ -0,0 +1,15 @@
+import Register from "../components/User-Auth/register";
+import Footer from "../components/landing_page/Footer";
+
+function SignUpView() {
+  return (
+    <>
+      <div className="bg-primary">
+        <Register />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default SignUpView;
