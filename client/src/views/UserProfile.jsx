ad9d39daeb4669db80097018eeb85741a24af6a9
jbrov2 <jafaribrownsha@gmail.com>
2024-07-01 14:35:56 -0400

    update

---
+++ client/src/views/UserProfile.jsx
@@ -0,0 +1,17 @@
+import Profile from '../components/Profile/profile'
+
+function UserProfile(){
+    return(
+    <>
+        <div className="bg-primary">
+            <Profile/>
+            <div className='h-[3000px]'>
+
+            </div>
+        </div>
+    </>  
+    )
+
+}
+
+export default UserProfile;
