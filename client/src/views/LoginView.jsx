cf228fde098b4cdff21fd4eb53cb974fac7adf31
jbrov2 <jafaribrownsha@gmail.com>
2024-07-01 10:05:55 -0400

    added footer and some styling

--- client/src/views/LoginView.jsx
+++ client/src/views/LoginView.jsx
@@ -1,4 +1,6 @@
 import Login from "../components/User-Auth/login";
+import Footer from "../components/landing_page/Footer";
+
 
 function LoginView() {
   return (
@@ -6,6 +8,7 @@ function LoginView() {
       <div className="bg-primary overflow-hidden">
         {/*Register Componenet */}
         <Login />
+        <Footer/>
       </div>
     </>
   );
--- client/src/views/SignUpView.jsx
+++ client/src/views/SignUpView.jsx
@@ -1,11 +1,12 @@
 import Register from "../components/User-Auth/register";
+import Footer from '../components/landing_page/Footer'
 
 function SignUpView() {
   return (
     <>
-      <div className="bg-primary overflow-hidden">
+      <div className="bg-primary ">
         <Register />
-        {/* <Footer /> */}
+        <Footer/>
       </div>
     </>
   );
