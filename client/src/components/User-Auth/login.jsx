1b9ae6d8296ffa48cb799fbaf4b8b5e11e6555d4
jbrov2 <jafaribrownsha@gmail.com>
2024-07-02 17:14:51 -0400

    fixed screen

--- client/src/components/User-Auth/login.jsx
+++ client/src/components/User-Auth/login.jsx
@@ -17,7 +17,7 @@ function Login() {
         <div className="top-0 flex flex-row justify-end p-4">
           <RiCloseCircleFill className="icon" onClick={handleExit} />
         </div>
-        <div className="max-w-[600px] mb-8 mx-4 md:h-screen pt-[100px] xl:mx-auto px-6 xl:px-0 h-[368] items-center">
+        <div className="max-w-[600px] mb-8 mx-4 h-screen pt-[100px] xl:mx-auto px-6 xl:px-0  items-center">
           {/*Container */}
           <div className="container mx-auto  xl:p-0">
             <h2 className="h2 mb-8"> Log in</h2>
