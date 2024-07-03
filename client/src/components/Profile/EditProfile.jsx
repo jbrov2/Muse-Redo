86edc63dd719724989c0feb705e9b97662e4f4ac
jbrov2 <jafaribrownsha@gmail.com>
2024-07-03 11:14:43 -0400

    made edit profile

--- client/src/components/Profile/EditProfile.jsx
+++ client/src/components/Profile/EditProfile.jsx
@@ -7,8 +7,42 @@ function EditProfile() {
         <div className="max-w-[1466px] mx-4 xl:mx-auto px-6 xl:px-0 items-center">
           <div className="container mx-auto">
             <h2 className="h2 mb-8">Profile</h2>
-            <div className="flex items-center">
-              <RiUser5Fill size={60} />
+            <div className="flex items-center xl:flex-row flex-col xl:gap-60 gap-12">
+              {/*Place Holder */}
+              <div className=" flex flex-col items-center">
+                <RiUser5Fill size={250} />
+                <p className="cursor-pointer">Change Icon</p>
+              </div>
+
+              <div className="flex flex-col ">
+                <form action="" className="flex flex-col">
+                  <label htmlFor="d_name" id="d_name">
+                    Display Name
+                  </label>
+                  <input type="text" id="d_name" placeholder="Go Crazy" />
+                  <label htmlFor="status" id="status">
+                    Status
+                  </label>
+                  <input
+                    type="text"
+                    id="status"
+                    placeholder="Type in your status"
+                  />
+                  <button type="submit" className="btn btn-primary mt-4">
+                    {" "}
+                    Submit all
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="flex flex-row xl:flex-col mt-20">
+              <form action="" className="flex flex-col w-full">
+                <h3 className="h3">Bio</h3>
+                <textarea name="bio" id="bio" rows="4"></textarea>
+                <button type="submit" className="btn btn-primary mt-8">
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
         </div>
