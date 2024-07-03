b9cc76d121a934b8e3b17beb8c8eb6863fe01f83
jbrov2 <jafaribrownsha@gmail.com>
2024-07-03 10:03:25 -0400

    edit profile page

--- client/src/components/Profile/EditProfile.jsx
+++ client/src/components/Profile/EditProfile.jsx
@@ -1,7 +1,18 @@
+import { RiUser5Fill } from "@remixicon/react";
+
 function EditProfile() {
   return (
     <>
-      <section></section>
+      <section className="edit_prof xl:h-screen xl:p-[200px]">
+        <div className="max-w-[1466px] mx-4 xl:mx-auto px-6 xl:px-0 items-center">
+          <div className="container mx-auto">
+            <h2 className="h2 mb-8">Profile</h2>
+            <div className="flex items-center">
+              <RiUser5Fill size={60} />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
