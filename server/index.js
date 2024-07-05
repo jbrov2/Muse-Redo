0e104edb16f28ec11b8be60a7a42c64663e1286c
jbrov2 <jafaribrownsha@gmail.com>
2024-07-05 14:19:37 -0400

    created server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config();
-
+const cors = require("cors");
+require("dotenv").config();
 
 //MIDDLEWARE
 
@@ -10,30 +9,26 @@ require('dotenv').config();
 app.use(express.json());
 
 //for form data
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 //Cross Origin
-app.use(cors({origin:"http://localhost:5173"}))
-
-
+app.use(cors({ origin: "http://localhost:5173" }));
 
 //Grabbing PORT
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
+const spotify_id = process.env.SPOTIFY_ID;
+const spotify_secret = process.env.SPOTIFY_SECRET;
+const redirect = process.env.REDIRECT_URI;
 
 //Routes
 
-app.get('/', (req,res) =>{
-    res.send('Testing')
-})
+app.get("/", (req, res) => {
+  res.send("Testing");
+});
 
 //Spotify
-app.use('/auth/login', require('./routes/api/spotify'));
-app.use("/auth/callback", require('./routes/api/spotify'));
-app.use('/auth/token', require('./routes/api/spotify'));
+app.use("/auth", require("./routes/api/spotify"));
 
-
-
-app.listen(PORT, () =>{
-    console.log(`LISTENING ON PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
