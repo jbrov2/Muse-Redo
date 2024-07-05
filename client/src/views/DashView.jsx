6c637a17ddbd508bd3c339870f69b6f918a89c24
jbrov2 <jafaribrownsha@gmail.com>
2024-07-05 08:43:20 -0400

    created dash view

---
+++ client/src/views/DashView.jsx
@@ -0,0 +1,11 @@
+function DashView() {
+  return (
+    <>
+      <div className="bg-primary">
+        <div className="h-[3000px]"></div>
+      </div>
+    </>
+  );
+}
+
+export default DashView;
